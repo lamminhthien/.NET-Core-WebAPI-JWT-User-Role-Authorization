b31776388f00e107763d53af0027b297fc2af3ce
Lâm Minh Thiện - 60136962 <lamminhthien02012000@gmail.com>
2022-02-23 11:04:42 +0700

    Add username to generate token

--- Models/UserInfoList.cs
+++ Models/UserInfoList.cs
@@ -6,8 +6,8 @@ namespace WebAPI__.NET_Core_JWT_User_Role_Authorization.Models
     {
         public static List<UserInfo> Users = new List<UserInfo>
         {
-            new UserInfo() {EmailAddress = "admin@gmail.com", Role="admin",Fullname="Lam Minh Thien",Password="hello_admin" },
-            new UserInfo() {EmailAddress= "thien@gmail.com",Role="seller",Fullname="Tran Quoc Thinh",Password="hello_seller"}
+            new UserInfo() {EmailAddress = "admin@gmail.com", Role="admin",Fullname="Lam Minh Thien",Password="hello_admin" ,Username="admin"},
+            new UserInfo() {EmailAddress= "thien@gmail.com",Role="seller",Fullname="Tran Quoc Thinh",Password="hello_seller",Username="thien"}
 
         };
     }
