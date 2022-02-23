081ce4d36b896f1a84b5059bc8de1cb7d7a5e534
Lâm Minh Thiện - 60136962 <lamminhthien02012000@gmail.com>
2022-02-23 08:47:56 +0700

    Create Model for login and model for carry userinfo along with request

---
+++ Models/UserAuthorize.cs
@@ -0,0 +1,8 @@
+﻿namespace WebAPI__.NET_Core_JWT_User_Role_Authorization.Models
+{
+    public class UserAuthorize
+    {
+        public string Username { get; set; }
+        public string Password { get; set; }
+    }
+}
---
+++ Models/UserInfo.cs
@@ -0,0 +1,10 @@
+﻿namespace WebAPI__.NET_Core_JWT_User_Role_Authorization.Models
+{
+    public class UserInfo
+    {
+        public string Fullname { get; set; }
+        public string EmailAddress { get; set; }
+        public string Role { get; set; }
+         
+    }
+}
