f5efd73f60148e078c0f2e2e7ee0840de3d6facb
Lâm Minh Thiện - 60136962 <lamminhthien02012000@gmail.com>
2022-02-23 10:11:02 +0700

    Hàm dịch JWS có trong client
     Khi client gửi request kèm JWS sang Server xử lý và trích xuất dữ liệu.
    Để chuẩn bị xác thực và cấp quyền,...

---
+++ Controllers/UserController.cs
@@ -0,0 +1,53 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Claims;
+using System.Threading.Tasks;
+using WebAPI__.NET_Core_JWT_User_Role_Authorization.Models;
+
+namespace WebAPI__.NET_Core_JWT_User_Role_Authorization.Controllers
+{
+    // Đặt route cho controller này "api/usercontroller"
+    [Route("api/[controller]")]
+    [ApiController]
+    // Dùng base Controller với MVC nhưng không cần hỗ trợ View
+    public class UserController : ControllerBase
+    {
+
+        [HttpGet("Admins")]
+        [Authorize(Roles = "admin")]
+        public IActionResult AdminsEndpoint()
+        {
+            //var currentUser = Ge
+            return Ok();
+        }
+
+
+        // Hàm đọc Token JWT vừa được tạo ra và lưu bên LocalStorage của web browser client
+        public UserInfo GetCurrentUserFromJWT_Client()
+        {
+            //Đọc tokken string từ dữ liệu truyền qua giao thức http
+            var identity = HttpContext.User.Identity as ClaimsIdentity;
+
+            // Trích xuất thông tin từ tokken ra
+            if (identity != null)
+            {
+                //Lấy mảng thông tin định danh người dùng
+                var userClaims = identity.Claims;
+                
+                // Lọc ra các dữ liệu, lưu vào UserInfo Object
+                return new UserInfo
+                {
+                    Username = userClaims.FirstOrDefault(o => o.Type == ClaimTypes.NameIdentifier)?.Value,
+                    EmailAddress = userClaims.FirstOrDefault(o => o.Type == ClaimTypes.Name)?.Value,
+                    Role = userClaims.FirstOrDefault(o => o.Type == ClaimTypes.Role)?.Value
+                };
+            }
+            return null;
+        }
+    }
+}
