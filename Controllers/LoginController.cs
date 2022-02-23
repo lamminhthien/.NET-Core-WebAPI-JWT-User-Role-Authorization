f26cf3f82bc6cbb936c3b5145c1b0171c865c0b1
Lâm Minh Thiện - 60136962 <lamminhthien02012000@gmail.com>
2022-02-23 09:35:01 +0700

    Hàm kiểm tra tài khoản với database và Hàm tạo JWT Token -> JWS (Chữ ký web JSON)

--- Controllers/LoginController.cs
+++ Controllers/LoginController.cs
@@ -33,7 +33,12 @@ namespace WebAPI__.NET_Core_JWT_User_Role_Authorization.Controllers
         {
             //Thực hiện bước kiểm tra xác thực
             var user = Authenticate(userAuthorize);
-
+            //Khi user có tồn tại, tiến hành tạo token
+            if (user != null)
+            {
+                var token =
+                    return Ok(token);
+            }
             return Ok();
 
         }
@@ -51,7 +56,37 @@ namespace WebAPI__.NET_Core_JWT_User_Role_Authorization.Controllers
 
             //Trả về null khi đăng nhập thất bại
             return null;
+
+        }
+
+        //Generate JWT chứa tất cả thông tin của người dùng
+        //từ table UserInfo
+        private string GenerateJWT_Token(UserInfo userInfo)
+        {
+            //Mã hoá secret key từ file appsetting.json
+            var securityKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(_config["Jwt:Key"]));
+
+            //Tạo chứng chỉ chứng thực từ secretkey trên với mã hoá Sha256 (Header của JWT)
+            var credentials = new SigningCredentials(securityKey, SecurityAlgorithms.HmacSha256);
+
+            //Tạo  Claim để mã hoá các thông tin trong database (Payload của JWT)
+            var claims = new[]
+            {
+                new Claim(ClaimTypes.NameIdentifier,userInfo.Username),
+                new Claim (ClaimTypes.Email,userInfo.EmailAddress),
+                new Claim (ClaimTypes.Name,userInfo.Fullname),
+                new Claim (ClaimTypes.Role,userInfo.Role)
+            };
+
+            // Tạo tokken cùng với các thông tin của Key "Jwt" trong appsetting.json
+            var token = new JwtSecurityToken(_config["Jwt:Issuer"], _config["Jwt:Audience"],
+                claims,
+                expires: DateTime.Now.AddMinutes(15),
+                signingCredentials: credentials);
+            // Serialize từ JWT sang JWE hoặc JWS (Chữ ký xác thực JSON)
+            return new JwtSecurityTokenHandler().WriteToken(token);
             
         }
+        
     }
 }
